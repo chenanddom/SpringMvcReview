1f4afdf96ea8f703404f793d61905f14a5bca81f
chenanddom <772571631@qq.com>
2018-12-23 21:37:24 +0800

    Initial commit

---
+++ src/main/java/com/flexible/WebInitializer.java
@@ -0,0 +1,30 @@
+package com.flexible;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: chendom
+ * Date: 2018-12-21
+ * Time: 14:49
+ */
+public class WebInitializer implements WebApplicationInitializer{
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebmvcConfig.class);
+        ctx.setServletContext(servletContext); //2
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx)); //3
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+        servlet.setAsyncSupported(true);//1
+    }
+}
---
+++ src/main/java/com/flexible/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.flexible.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: chendom
+ * Date: 2018-12-21
+ * Time: 14:55
+ */
+@Controller
+public class DemoController {
+@RequestMapping("/index")
+public String index(){
+    return "index";
+}
+}
